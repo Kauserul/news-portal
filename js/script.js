71d682a00d8098b0e77a7b720370f3469498b664
Kauserul <kauserulislam0055@gmail.com>
2022-09-05 23:45:20 +0600

    all new

--- js/script.js
+++ js/script.js
@@ -1,103 +1,37 @@
 
-const menuBar = async() =>{
+const loadMenuBar = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
-    const data = await res.json();
+    const data = await res.json()
+    .catch(error => console.log(error))
     displayMenuBar(data.data.news_category)
-}
-
-const displayMenuBar = lists=>{
-    // console.log(lists)
-    const menuBar = document.getElementById('menu-bar');
-    lists.forEach(list =>{
-        // console.log(list)
-        const menuDiv = document.createElement('div');
-        menuDiv.innerHTML = `
-        <div>
-            <a href="#" class="me-5 cetagory">${list.category_name}</a>
-        </div>
-        `;
-        menuBar.appendChild(menuDiv)
-    })
 };
 
+const displayMenuBar = cetagorys =>{
+    // console.log(cetagorys)
+    const topNav = document.getElementById('navbarNavAltMarkup');
+    for(const cetagory of cetagorys){
+        console.log(cetagory)
+        const navBarMenu = document.createElement('div');
+        navBarMenu.classList.add('navbar-nav')
+        navBarMenu.innerHTML = `
+            <a onclick="loadCetagoryDetails('${cetagory.category_id}')" class="nav-link me-4 ms-3" href="#">${cetagory.category_name}</a>
+        `
+        topNav.appendChild(navBarMenu)
+    }
+};
 
-const loadNews = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+const loadCetagoryDetails = async(id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
     const res = await fetch(url);
-    const data = await res.json();
-    dispalyNews(data.data)
+    const data = await res.json()
+    .catch(error => console.log(error));
+   displayCetagoryDetails(data)
 };
 
-const dispalyNews = datas => {
-    // console.log(datas)
-    const mainCard = document.getElementById('main-card');
-    datas.forEach(data => {
-        console.log(data)
-        const div = document.createElement('div');
-        div.classList.add('col-lg-12');
-        div.innerHTML = `
-        <div class="card mb-3" style="max-width: 1040px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img style="height: 250px" src="${data.image_url}" class="img-fluid rounded-start p-2" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${data.title}</h5>
-              <p class="card-text">${(data.details).slice(0, 300)}</p>
-              
-
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex">
-                    <img class="author-pic rounded me-2" src ="${data.author.img}">
-                    <div>
-                        <p class="mb-0 small">${data.author.name}</p>
-                        <p class="mt-0 small">${data.author.published_date}</p>
-                    </div>
-                </div>
-
-                <div class="pb-0">
-                    <span><i class="fas fa-solid fa-eye me-1"></i>${data.total_view}</span>
-                </div>
-
-                <div>
-                    <i class="fas fa-regular fa-star"></i>
-                    <i class="fas fa-regular fa-star"></i>
-                    <i class="fas fa-regular fa-star"></i>
-                    <i class="fas fa-regular fa-star"></i>
-                    <i class="fas fa-regular fa-star"></i>
-                </div>
-
-                <div onclick="moreDetails()">
-                    <button onclick="displayMoreDetails('${data._id}')" class="btn btn-primary data-bs-toggle="modal" data-bs-toggle="modal" data-bs-target="#exampleModal">See More</button>
-                </div>
-                
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-        `;
-        mainCard.appendChild(div)
-        
-    });
-    
-};
-
-const displayMoreDetails = news_id =>{
-    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayNewsDetails(data.data[0]._id))
+const displayCetagoryDetails = details =>{
+    console.log(details)
 }
 
-const displayNewsDetails = singleNewsDetails =>{
-      console.log(singleNewsDetails)
-      const newsTitle = document.getElementById('exampleModalLabel');
-      newsTitle.innerText = singleNewsDetails.title
-}
-
-menuBar();
-loadNews()
+loadCetagoryDetails()
+loadMenuBar()
