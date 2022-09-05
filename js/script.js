ae548a1ab89a96234761065520fc497d18bc8b20
Kauserul <kauserulislam0055@gmail.com>
2022-09-06 03:53:34 +0600

    done

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const displayMenuBar = cetagorys =>{
         const navBarMenu = document.createElement('div');
         navBarMenu.classList.add('navbar-nav')
         navBarMenu.innerHTML = `
-            <a onclick="loadCategoryDetails('${cetagory.category_id}')" class="nav-link me-4 ms-3" href="#">${cetagory.category_name}</a>
+            <a onclick="loadCategoryDetails('${cetagory.category_id}')" class="nav-link me-4 ms-3 text-dark" href="#">${cetagory.category_name}</a>
         `
         topNav.appendChild(navBarMenu)
     }
@@ -32,26 +32,101 @@ const loadCategoryDetails = (id)=>{
 const displayCetagoryDetails = details =>{
     console.log(details);
     const displayNews = document.getElementById('display-news');
-    for(const news of details){
+    for(const detail of details){
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card', 'mb-3')
         newsDiv.innerHTML =`
-        <div class="row g-0">
-        <div class="col-md-4">
-          <img src="${news.image_url}" class="img-fluid rounded-start p-3" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">${news.title}</h5>
-            <p class="card-text">${(news.details).slice(0, 450)}</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-      </div>
+        <div class="col-lg-12 col-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-12 col-md-12 d-flex pb-3 ">
+                            <img class="mx-auto"  src="${detail.thumbnail_url}" alt="" >
+                        </div>
+
+                        <div class="col-lg-9 p-4">
+
+                              <h4 class="card-title">${detail.title}</h4>
+                                 <p class="card-text my-3">${detail.details.slice(0, 450) + ' ' + 'more.....'}</p>
+
+                            
+                                 <div class="row d-flex align-items-center justify-content-center ">
+                                       <div class="col-md-4 d-block align-items-center"">
+                                     <img  class="w-25 rounded-circle me-3 mx-auto pb-2" src="${detail.author.img}" alt="">
+                                 <div>
+                                     <h6>${detail.author.name ? detail.author.name : 'NO Name Found'}</h6>
+                                    <p class= "py-2"> ${detail.author.published_date ? detail.author.published_date : 'NO Date Found'}</p>
+                                        </div>
+                       
+                                     </div>
+                                         <div class="col-md-3 d-flex ">
+                                          <h5 class='mx-2 class= "py-2"' ><i class="fas fa-regular fa-eye"></i></h5>
+                                         <h5 > ${detail.total_view ? detail.total_view : 'not view'}</h5>
+                                 </div>
+                                <div class="col-lg-3 col-sm-12 d-flex py-2">
+                                   <h5><i class="fas fa-solid fa-star"></i></h5>
+                                   <h5><i class="fas fa-solid fa-star"></i></h5>
+                                   <h5><i class="fas fa-solid fa-star"></i></h5>
+                                   <h5><i class="fas fa-regular fa-star"></i></h5>
+                                   <h5><i class="fas fa-regular fa-star"></i></h5>
+                                </div>
+
+                                 <div class="col-lg-2 d-flex">         
+                                 
+                                   <button type="button" onclick="modalDetails('${detail._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                   more view</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
         `;
         displayNews.appendChild(newsDiv)
     }
 
+}
+
+const modalDetails = id =>{
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(data => displayModalDetails(data))
+        .catch(error => displayModalDetails(error))
+}
+
+const displayModalDetails = details =>{
+    const modal = document.getElementById('exampleModalLabel');
+    modal.innerHTML = ''
+
+    try {
+        const creatediv = document.createElement('div');
+
+        creatediv.innerHTML = `
+                            <div class="row">
+                                 <div class="col-lg-12">
+                                         <img class="w-100"  src="${details.data[0].image_url}" alt="" >
+                                         <h1 class="card-text my-3">${details.data[0].title}</h1>
+                                         <p class = 'text'>${details.data[0].details.slice(0, 250) + ' ' + 'more.....'}</p>
+                                         <h5 h5 > ${details.data[0].author.published_date ? details.data[0].author.published_date : 'No published Date'}</h5 >
+                                         <div class= "d-flex align-items-center justify-content-center ">
+                                           <div class = "mx-auto">
+                                           <img class="w-25 rounded-circle my-2 d-block"  src="${details.data[0].author.img}" alt="" >
+                                           <p class = 'text '>${details.data[0].author.name ? details.data[0].author.name : 'No author Name!'}</p>
+                                           </div>
+                                         </div>
+                                         </div>
+                                         <div class="col-md-3 d-flex ">
+                                          <h5 class='mx-2 class= "py-2"' ><i class="fas fa-regular fa-eye"></i></h5>
+                                         <h5 > ${details.data[0].total_view ? details.data[0].total_view : 'not view'}</h5>
+                                 </div>
+                             </div>
+                             </div>
+     `;
+        modal.appendChild(creatediv);
+
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 loadCategoryDetails()
